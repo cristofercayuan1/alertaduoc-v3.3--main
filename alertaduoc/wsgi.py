e952fd750578fbbe9be37e018d414944c5404670
cristofercayuan1 <cr.cayuan@duocuc.cl>
2024-07-02 22:44:49 -0400

    3.6

--- alertaduoc/wsgi.py
+++ alertaduoc/wsgi.py
@@ -1,16 +1,22 @@
 """
 WSGI config for alertaduoc project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
+It exposes the WSGI callable as a module-level variable named 'application'.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alertaduoc.settings')
+# Añade la ruta del proyecto a sys.path
+path = os.path.expanduser('~/alertaduoc-v3.6--main')
+if path not in sys.path:
+    sys.path.insert(0, path)
 
-application = get_wsgi_application()
+# Configura la variable de entorno para Django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'alertaduoc.settings'
+
+# Obtén la aplicación WSGI de Django
+application = StaticFilesHandler(get_wsgi_application())
